2a0a18b19754e84ad035a88784a3597da56098ee
satwik77 <satwik55@gmail.com>
2015-09-20 23:32:55 +0530

    documenting certi-gen.py

--- certi-generator/certi-gen.py
+++ certi-generator/certi-gen.py
@@ -5,34 +5,39 @@ import os
 
 
 for t in range(1,54):
-	fopen = open('%d' % t, 'r')
+	fopen = open('%d' % t, 'r')   # open input files from 1 to 53 
 	# gopen = open('%d' % t, 'r')
-	for z in range(100):
+	for z in range(100):               #max 100 participants from a college so breaks when it ends before that
 		rline = fopen.readline()
 
 		if rline:
 			print rline
-			plist = rline.split('$')
+			plist = rline.split('$') 
+			# a sample line : 'Kishan Kumar $ College of Vocational Studies $ Volleyball (Boys)'
 
 			print plist
 			newpath = '/home/gauss/Git proj/Certificate-Generator/output/%s/' % plist[1].replace(" ", "")
 			print newpath
+			# plist[0] contains name. plist[1] contains collegename and plist[2] contains events as per the sample input files
 
 			if not os.path.exists(newpath): 
 				clg = plist[1].replace(' ','')
 				os.mkdir('/home/gauss/Git proj/Certificate-Generator/output/%s/' % clg)
 			elist = plist[2].split(',')
 			elist1= []
+
+			# Removes (boys) or (girls) from '[any_sport] (boys)' like 'volleyball (boys)'
+
 			for t in elist:
 				if 'BOYS' in t.upper():
 					t = t.upper()
-					t = t[:t.find(" (BOYS)")]
+					t = t[:t.find(" (BOYS)")]    # new string without '(BOYS) '
 					t=t.strip()
 					elist1.append(t.title())
 				elif 'GIRLS' in t.upper():
 					t = t.upper()
 					t=t.strip()
-					t = t[:t.find(" (GIRLS)")]
+					t = t[:t.find(" (GIRLS)")]    # new string without '(GIRLS)'
 					elist1.append(t.title())
 				else: 
 					t=t.strip()
@@ -43,12 +48,12 @@ for t in range(1,54):
 			event_str= ','.join(elist1)
 			print event_str
 
-			#name correction
+			#name correction 
 			temp = [x.title() for x in plist[0].split(" ")]
 			plist[0] = " ".join(temp)
 
 			with Drawing() as draw:
-				with Image(filename='Participation.jpg') as image:
+				with Image(filename='Certi-template.jpg') as image:  # replace filename of template here
 					draw.font = 'FoglihtenNo07_0841.otf'
 					draw.font_size = 60
 					draw.text_alignment ='center'
